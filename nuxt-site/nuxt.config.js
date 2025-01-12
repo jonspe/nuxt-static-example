79d189db492cc589049d58dcecff1ce804d0ac02
Joona Perasto <jonspe.pe@gmail.com>
2025-01-12 17:01:44 +0200

    Working static rendering on nuxt

--- nuxt-site/nuxt.config.js
+++ nuxt-site/nuxt.config.js
@@ -1,10 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+async function getBlogRoutes() {
+  const data = await fetch("https://jsonplaceholder.typicode.com/posts")
+  const posts = await data.json()
+  return posts.map((post) => `/blog/${post.id}`)
+}
+
 export default defineNuxtConfig({
   routeRules: {
     "/**": { prerender: true },
   },
+  ssr: true,
+  target: "static",
   devtools: {
-    enabled: false,
+    enabled: true,
   },
   app: {
     pageTransition: { name: "page", mode: "in" },
@@ -12,4 +20,12 @@ export default defineNuxtConfig({
   srcDir: "src/",
   css: ["@/assets/css/globals.css"],
   compatibilityDate: "2025-01-12",
+  hooks: {
+    async "prerender:routes"(ctx) {
+      const routes = await getBlogRoutes()
+      for (const route of routes) {
+        ctx.routes.add(route)
+      }
+    },
+  },
 })
